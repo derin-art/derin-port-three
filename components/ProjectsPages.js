cb09769e6026919ab18eefb07e7664d15b464949
Derin Owoade <owoadederin6@gmail.com>
2023-01-09 20:44:14 +0100

    Changes to cusor

--- components/ProjectsPages.js
+++ components/ProjectsPages.js
@@ -21,11 +21,11 @@ export default function ProjectsPages() {
     </svg>
   );
   const ProjectNames = [
+    { name: "Landing Page", href: `/landing`, img: Land1 },
     { name: "E-Commerce", href: `/E-com`, img: Ecom2 },
     { name: "E-Com Management system", href: "/Crud", img: Vert2 },
 
     { name: "AGILE/Scrum app", href: `/Agile`, img: agile2 },
-    { name: "Landing Page", href: `/landing`, img: Land1 },
   ];
   const finalArray = [];
   const chunkSize = 2;
@@ -39,11 +39,15 @@ export default function ProjectsPages() {
     <div className="flex flex h-screen justify-center ">
       <div className="w-full relative md:p-8 p-4 pt-8">
         <div className="w-full pt-8 flex justify-around relative ">
-          <motion.div className="text-black font-Ezcar sticky top-10 self-start  2xl:text-5xl lg:text-5xl xl:text-5xl z-50  md:text-5xl text-xl">
+          <motion.div className="text-black hidden font-Ezcar lg:block sticky top-10 self-start  2xl:text-5xl lg:text-5xl xl:text-5xl z-50  md:text-5xl text-xl">
             PROJECTS
             <div className="blurry-gradientII -top-4"></div>
           </motion.div>
           <div className="">
+            <motion.div className="text-black font-Ezcar lg:hidden flex items-center justify-center top-10 self-start  2xl:text-5xl lg:text-5xl xl:text-5xl z-50  md:text-5xl text-xl">
+              PROJECTS
+              <div className="blurry-gradientII -top-4"></div>
+            </motion.div>
             {finalArray.map((item, index) => {
               return (
                 <div
